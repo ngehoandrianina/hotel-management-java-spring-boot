936b836a92a09092041b0acd3945b1ca68fcbd4e
Antonio Claret <antonioclaret2004@gmail.com>
2026-08-29 17:28:33 +0300

    Update Room entity to use AUTO generation strategy for ID and add version to H2 dependency in pom.xml

--- src/main/java/com/hotel/management/entity/Room.java
+++ src/main/java/com/hotel/management/entity/Room.java
@@ -22,7 +22,7 @@ import java.util.List;
 public class Room {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
 
     @NotBlank(message = "Le numero de chambre est obligatoire")
